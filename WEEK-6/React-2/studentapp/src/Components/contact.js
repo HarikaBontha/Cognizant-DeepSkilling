11b2d5db1336da236085496d22a1c7cb15f89ae9
HarikaBontha <162826965+HarikaBontha@users.noreply.github.com>
2025-07-26 09:22:40 +0530

    Add files via upload

---
+++ WEEK-6/React-2/studentapp/src/Components/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function contact() {
+  return (
+    <div>
+      <h2>Welcome to the Contact page of the Student Management Portal</h2>
+    </div>
+  );
+}
+
+export default contact;
